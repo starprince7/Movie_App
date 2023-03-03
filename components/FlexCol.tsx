58eb27f10d32cbc6a289b6345a4984499f96aa34
starprince7 <princeagezinweke@gmail.com>
2023-03-03 04:17:05 +0100

    fix: displayName definition.

--- components/FlexCol.tsx
+++ components/FlexCol.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Box, BoxProps, PaperProps } from "@mui/material";
 
-export const FlexCol = React.forwardRef(({ children, sx, ...rest }: BoxProps & Partial<PaperProps>, ref) => {
-  return (
-    <Box ref={ref} {...rest} sx={{ display: "flex", flexDirection: "column", ...sx }}>
-      {children}
-    </Box>
-  );
-});
+export const FlexCol = React.forwardRef(
+  ({ children, sx, ...rest }: BoxProps & Partial<PaperProps>, ref) => {
+    return (
+      <Box
+        ref={ref}
+        {...rest}
+        sx={{ display: "flex", flexDirection: "column", ...sx }}
+      >
+        {children}
+      </Box>
+    );
+  },
+);
+
+FlexCol.displayName = "FlexCol";
